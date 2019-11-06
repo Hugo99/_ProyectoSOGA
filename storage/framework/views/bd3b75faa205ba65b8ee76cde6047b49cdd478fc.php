f5ff3cb3777e5a6258639869506f08170add4486
Hugo Eguino <a217201885@alumnos.unison.mx>
2019-11-05 21:07:04 -0700

    Notificaciones

--- storage/framework/views/bd3b75faa205ba65b8ee76cde6047b49cdd478fc.php
+++ storage/framework/views/bd3b75faa205ba65b8ee76cde6047b49cdd478fc.php
@@ -5,15 +5,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     <title><?php echo $__env->yieldContent('title'); ?></title>
     <style>
+    .toast-info {
+      background-color: #2f96b4;
+      background-image: url(https://github.com/iconic/open-iconic/blob/master/png/bell-2x.png?raw=true)!important;
+    }
+
     .sidebar {
       margin: 0;
       padding: 0;
@@ -75,13 +78,16 @@
 
                 </a>
             </div>
-            
-    <script>
+
+            <button class="fa fa-bell-o btn btn-primary" type="button" id="notf"></button>
+
+    <!--<script>
     $(document).ready(function(){
       $('.toast').toast('show');
     });
-    </script>
+    </script>-->
     </div>
+
 
 <div class="">
     <div class="sidebar col-md-3" >
@@ -104,9 +110,22 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+
+    const alerta = document.querySelector('#notf');
+
+    alerta.addEventListener('click',() => {
+      toastr.info('Hola','prueva',{
+        "closeButton": true,
+      });
+
+    })
+    </script>
+
   </body>
 </html>
 <?php /**PATH /Users/hugoeguino/_ProyectoSOGA/_ProyectoSOGA/resources/views/sidebar.blade.php ENDPATH**/ ?>
