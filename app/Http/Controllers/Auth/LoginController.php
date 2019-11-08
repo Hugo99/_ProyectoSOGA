6a340e53ba6e20e62fb9cbf7191be30120875aa3
Hugo Eguino <a217201885@alumnos.unison.mx>
2019-11-08 08:36:59 -0700

    Prop

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,29 +40,6 @@ class LoginController extends Controller
     protected function authenticated(Request $request, $user)
     {
 
-      // If the class is using the ThrottlesLogins trait, we can automatically throttle
-      // the login attempts for this application. We'll key this by the username and
-      // the IP address of the client making these requests into this application.
-      if ($lockedOut = $this->hasTooManyLoginAttempts($request)) {
-          $this->fireLockoutEvent($request);
-
-          return $this->sendLockoutResponse($request);
-      }
-
-      $credentials = $this->credentials($request);
-
-      if ($this->guard()->attempt(['email' => $request->email, 'password' => $request->password, 'activated' => 1], $request->has('remember'))) {
-          return $this->sendLoginResponse($request);
-      }
-
-      // If the login attempt was unsuccessful we will increment the number of attempts
-      // to login and redirect the user back to the login form. Of course, when this
-      // user surpasses their maximum number of attempts they will get locked out.
-      if (! $lockedOut) {
-          $this->incrementLoginAttempts($request);
-      }
-
-      return $this->sendFailedLoginResponse($request);
     }
 
 
--- storage/framework/views/c029aa5c486daff2c77fe504c97fce7d43337291.php
+++ storage/framework/views/c029aa5c486daff2c77fe504c97fce7d43337291.php
@@ -1,19 +1,58 @@
-<?php $__env->startSection('content'); ?>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"><?php echo e(__('Login')); ?></div>
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
 
-                <div class="card-body">
-                    <form method="POST" action="<?php echo e(route('login')); ?>">
-                        <?php echo csrf_field(); ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right"><?php echo e(__('E-Mail Address')); ?></label>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control <?php $__errorArgs = ['email'];
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+<style media="screen">
+    body {
+      font: 15px/22px "Lucida Grande", Tahoma, Verdana, sans-serif;
+      position: relative;
+      margin: 30px auto;
+      padding: 20px 20px 20px;
+      width: 310px;
+    }
+
+    .card-header{
+        font-size: 30px;
+    }
+
+    .card-body{
+        text-align: left;
+        padding-top: 20px;
+    }
+</style>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"><?php echo e(__('Bienvenido a SOGA')); ?></div>
+                    <div class="card-body">
+                        <form method="POST" action="<?php echo e(route('login')); ?>">
+                            <?php echo csrf_field(); ?>
+
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right"><?php echo e(__('User')); ?></label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control <?php $__errorArgs = ['email'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
@@ -22,26 +61,26 @@ if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>" name="email" value="<?php echo e(old('email')); ?>" required autocomplete="email" autofocus>
 
-                                <?php $__errorArgs = ['email'];
+                                    <?php $__errorArgs = ['email'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong><?php echo e($message); ?></strong>
-                                    </span>
-                                <?php unset($message);
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong><?php echo e($message); ?></strong>
+                                        </span>
+                                    <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right"><?php echo e(__('Password')); ?></label>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right"><?php echo e(__('Password')); ?></label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control <?php $__errorArgs = ['password'];
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control <?php $__errorArgs = ['password'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
@@ -50,55 +89,54 @@ if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>" name="password" required autocomplete="current-password">
 
-                                <?php $__errorArgs = ['password'];
+                                    <?php $__errorArgs = ['password'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong><?php echo e($message); ?></strong>
-                                    </span>
-                                <?php unset($message);
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong><?php echo e($message); ?></strong>
+                                        </span>
+                                    <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>>
-
-                                    <label class="form-check-label" for="remember">
-                                        <?php echo e(__('Remember Me')); ?>
-
-                                    </label>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <?php echo e(__('Login')); ?>
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>>
 
-                                </button>
+                                        <label class="form-check-label" for="remember">
+                                            <?php echo e(__('Remember Me')); ?>
 
-                                <?php if(Route::has('password.request')): ?>
-                                    <a class="btn btn-link" href="<?php echo e(route('password.request')); ?>">
-                                        <?php echo e(__('Forgot Your Password?')); ?>
-
-                                    </a>
-                                <?php endif; ?>
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        <?php echo e(__('Login')); ?>
+
+                                    </button>
+
+                                    <?php if(Route::has('password.request')): ?>
+                                        <a class="btn btn-link" href="<?php echo e(route('password.request')); ?>">
+                                            <?php echo e(__('Forgot Your Password?')); ?>
+
+                                        </a>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts/app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/hugoeguino/_ProyectoSOGA/_ProyectoSOGA/resources/views//auth/login.blade.php ENDPATH**/ ?>
+</body>
+<?php /**PATH /Users/hugoeguino/_ProyectoSOGA/_ProyectoSOGA/resources/views//auth/login.blade.php ENDPATH**/ ?>
